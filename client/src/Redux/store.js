b684a1021219b9d12133ddb0e9b4ac5d0456e53e
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-16 16:29:41 +0530

    Restaurant Reducer

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./Reducer/rootReducer";
 
-const store = createStore(rootReducer);
+const middlewares = [thunk];
+
+if(process.env.NODE_ENV === "development") {
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
+
+const store = createStore(rootReducer, {}, applyMiddleware(...middlewares));
 
 export default store;
